f40a4f4fdadd86802e41da6fe9fad457247b7255
Rooparam Choudhary <roopchou@microsoft.com>
2019-09-17 15:50:02 +0530

    zoom levels for image

--- src/components/ImageCanvas.svelte
+++ src/components/ImageCanvas.svelte
@@ -2,27 +2,39 @@
 	import { tick } from 'svelte';
 	import { imageAnnotations } from '../stores/annotationsstore.js';
 
-	let imageEl;
-	let lastAdjustedImage = '';
+	const zoomLevels = [
+		{ id: 0, text: 'Page Width' },
+		{ id: 0.5, text: '50%' },
+		{ id: 1, text: '100%' },
+		{ id: 2, text: '200%' },
+		{ id: 4, text: '400%' }
+	];
+	let selectedZoomLevel;
 
-	const adjustImageDimensions = async () => {
-		await tick();
-		const imageName = `url(${$imageAnnotations.imageName})`;
-		if (imageName === lastAdjustedImage) {
-			return;
+	let imageEl;
+	let viewportWidth;
+
+	const setZoomLevel = (zoomLevel) => {
+		let ratio = viewportWidth / $imageAnnotations.imageWidth;
+		if (zoomLevel.id !== 0) {
+			ratio = zoomLevel.id;
 		}
-		imageEl.style["background-image"] = imageName;
-		const viewportWidth = imageEl.clientWidth;
-		const ratio = imageEl.clientWidth / $imageAnnotations.imageWidth;
 		imageEl.style["transform"] = `scale(${ratio})`;
 		imageEl.style["width"] = `${$imageAnnotations.imageWidth}px`;
 		imageEl.style["height"] = `${$imageAnnotations.imageHeight}px`;
-		lastAdjustedImage = imageName;
+	};
+
+	const initCanvas = async () => {
+		if (viewportWidth) { return; }
+		await tick();
+		imageEl.style["background-image"] = `url(${$imageAnnotations.imageName})`;
+		viewportWidth = imageEl.clientWidth;
+		setZoomLevel(zoomLevels[0]);	// page-width level
 	};
 
 	imageAnnotations.subscribe(value => {
 		if (value.loaded) {
-			adjustImageDimensions();
+			initCanvas();
 		}
 	});
 </script>
@@ -33,6 +45,11 @@
 	}
 </style>
 
+<select class="bg-gray-100" bind:value={selectedZoomLevel} on:change="{() => setZoomLevel(selectedZoomLevel)}">
+	{#each zoomLevels as zoomLevel}
+		<option value={zoomLevel}>{zoomLevel.text}</option>
+	{/each}
+</select>
 <div class="w-full h-full" bind:this={imageEl}>
 	<slot></slot>
 </div>

5f5b4349978a4f2b75defe2ffc679018423c5ec0
Rooparam Choudhary <roopchou@microsoft.com>
2019-09-17 18:41:04 +0530

    toggle rectangles

--- src/components/ImageCanvas.svelte
+++ src/components/ImageCanvas.svelte
@@ -1,15 +1,22 @@
 <script>
-	import { tick } from 'svelte';
+	import { tick, createEventDispatcher } from 'svelte';
 	import { imageAnnotations } from '../stores/annotationsstore.js';
 
+	const dispatch = createEventDispatcher();
+
 	const zoomLevels = [
-		{ id: 0, text: 'Page Width' },
-		{ id: 0.5, text: '50%' },
-		{ id: 1, text: '100%' },
-		{ id: 2, text: '200%' },
-		{ id: 4, text: '400%' }
+		{ id: 0, text: 'Screen Width' },
+		{ id: 0.5, text: 'Half Size' },
+		{ id: 1, text: 'Actual Size' },
+		{ id: 2, text: '2x' },
+		{ id: 4, text: '4x' }
 	];
 	let selectedZoomLevel;
+	export let showAnnotations = true;
+
+	const handleCheckboxToggle = (_event) => {
+		dispatch('toggleRectangleVisibility', { show: showAnnotations });
+	};
 
 	let imageEl;
 	let viewportWidth;
@@ -48,11 +55,20 @@
 	}
 </style>
 
-<select class="bg-gray-100" bind:value={selectedZoomLevel} on:change="{() => setZoomLevel(selectedZoomLevel)}">
-	{#each zoomLevels as zoomLevel}
-		<option value={zoomLevel}>{zoomLevel.text}</option>
-	{/each}
-</select>
+<div class="flex items-stretch justify-between">
+	<span class="self-start">
+		<label class="label">Zoom Level: </label>
+		<select class="bg-gray-100" bind:value={selectedZoomLevel} on:change="{() => setZoomLevel(selectedZoomLevel)}">
+			{#each zoomLevels as zoomLevel}
+				<option value={zoomLevel}>{zoomLevel.text}</option>
+			{/each}
+		</select>
+	</span>
+	<span class="self-end">
+		<label class="label">Show Annotations: </label>
+		<input type="checkbox" bind:checked={showAnnotations} on:change="{() => handleCheckboxToggle()}" />
+	</span>
+</div>
 <div class="h-full w-full overflow-scroll" id="container">
 	<div class="h-full overflow-x-scroll" id="canvas" bind:this={imageEl}>
 		<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,17 @@
 	import ImageCanvas from  '../components/ImageCanvas.svelte';
 	import Annotations from  '../components/Annotations.svelte';
 	import { imageAnnotations } from '../stores/annotationsstore.js';
+
+	let showAnnotations = true;
 </script>
 
 {#if !$imageAnnotations.loaded}
 	<div class="text-center w-full">Loading ...</div>
 {:else}
 	<ReadonlyText />
-	<ImageCanvas>
-		<Annotations />
+	<ImageCanvas {showAnnotations} on:toggleRectangleVisibility="{ (e) => showAnnotations = e.detail.show }" >
+		{#if showAnnotations}
+			<Annotations />
+		{/if}
 	</ImageCanvas>
 {/if}

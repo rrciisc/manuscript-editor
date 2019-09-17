2e5580b5fc5098650d937130fd3b19ea3b40510b
Rooparam Choudhary <roopchou@microsoft.com>
2019-09-18 00:56:00 +0530

    input box and attempt to create a line

--- src/components/CharRectangle.svelte
+++ src/components/CharRectangle.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { tick, onMount } from 'svelte';
 	import { selectedRectangleIdx } from '../stores/annotationsstore.js';
 
 	export let top;
@@ -7,8 +8,61 @@
 	export let height;
 	export let idx;
 
-	let box;
+	let box; let inputBox; let line;
 	let boxClass = '';
+
+	const createLineElement = (x, y, length, angle) => {
+		const line = document.createElement("div");
+		const styles = 'width: ' + length + 'px; '
+               + '-moz-transform: rotate(' + angle + 'rad); '
+               + '-webkit-transform: rotate(' + angle + 'rad); '
+               + '-o-transform: rotate(' + angle + 'rad); '  
+               + '-ms-transform: rotate(' + angle + 'rad); '  
+               + 'position: absolute; '
+               + 'top: ' + y + 'px; '
+							 + 'left: ' + x + 'px; '
+							 + 'padding: 0px; margin: 0px; height: 1px; line-height: 1px; background-color: greenyellow; ';
+    line.setAttribute('style', styles);  
+    return line;
+	};
+
+	const createLine = (x1, y1, x2, y2) => {
+		let a = x1 - x2;
+		let b = y1 - y2;
+		let c = Math.sqrt(a * a + b * b);
+
+		let sx = (x1 + x2) / 2;
+		let sy = (y1 + y2) / 2;
+
+		let x = sx - c / 2;
+		let y = sy;
+		let alpha = Math.PI - Math.atan2(-b, a);
+
+		return createLineElement(x, y, c, alpha);
+	};
+
+	// TODO: below connected line code have some issue so not using
+	const bringTextIntoFocus = async () => {
+		if ($selectedRectangleIdx === idx) {
+			boxClass = 'focus';
+			if (box) {
+				box.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+				await tick();
+				if (inputBox && !line) {
+					const aRect = box.getBoundingClientRect();
+					const bRect = inputBox.getBoundingClientRect();
+					const offsetY = bRect.top + bRect.height - aRect.top;
+					const offsetX = bRect.left - aRect.left;
+					line = createLine(aRect.width, 0, offsetX, offsetY);
+					await tick();
+					box.appendChild(line);
+				}
+			}
+		} else {
+			boxClass = '';
+		}
+	};
+
 	$: if ($selectedRectangleIdx === idx) {
 		boxClass = 'focus';
 		box && box.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
@@ -28,9 +82,17 @@
 	.focus {
 		border-color: greenyellow;
 	}
+
+	input {
+		z-index: 10;
+	}
 </style>
 
 <div class="{boxClass}"
 		style="top: {top}px; left: {left}px; width: {width}px; height: {height}px;"
 		bind:this={box}
-	></div>
+	>
+		<input class="appearance-none {boxClass === 'focus' ? '' : 'hidden'} leading-tight w-12 h-12 px-1 py-1 absolute bg-purple-200"
+			style="top: -6rem; left: 3rem"
+			type="text" bind:this={inputBox} />
+</div>
